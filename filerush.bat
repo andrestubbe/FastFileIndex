fa35a2c2a3df8de330cbf7eb09ad17fa131f1ff1
Andre Stubbe <andrestubbe@proton.me>
2026-04-25 05:41:14 +0200

    Add compilation step to filerush.bat

--- filerush.bat
+++ filerush.bat
@@ -1,3 +1,4 @@
 @echo off
+javac -cp target\fastfileindex-v1.0.0.jar examples\FileRush\src\main\java\fastfileindex\FileRush.java -d examples\FileRush\src\main\java
 cd build
 java --enable-native-access=ALL-UNNAMED -cp ..\target\fastfileindex-v1.0.0.jar;..\examples\FileRush\src\main\java fastfileindex.FileRush
