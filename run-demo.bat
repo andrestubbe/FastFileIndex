9ccf8528eb7de32d544bade6c48591cef3a13fb1
Andre Stubbe <andrestubbe@proton.me>
2026-04-25 06:04:47 +0200

    Simplify run-demo.bat to remove extra echo messages

--- run-demo.bat
+++ run-demo.bat
@@ -1,23 +1,4 @@
 @echo off
-echo === FastFileIndex Demo ===
-echo.
-
-REM Compile Demo
-echo Compiling Demo...
 javac -cp target\fastfileindex-v1.0.0.jar examples\Demo\src\main\java\fastfileindex\Demo.java -d examples\Demo\src\main\java
-if errorlevel 1 (
-    echo Compilation failed!
-    pause
-    exit /b 1
-)
-
-REM Run Demo from build directory
-echo.
-echo Running Demo...
 cd build
-java -cp ..\target\fastfileindex-v1.0.0.jar;..\examples\Demo\src\main\java fastfileindex.Demo
-cd ..
-
-echo.
-echo === Demo Complete ===
-pause
+java --enable-native-access=ALL-UNNAMED -cp ..\target\fastfileindex-v1.0.0.jar;..\examples\Demo\src\main\java fastfileindex.Demo
