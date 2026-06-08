745d4f013a84eb7427b11b2ba718d8bacc15e405
Andre Stubbe <andrestubbe@proton.me>
2026-06-08 04:56:05 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -1,15 +1,11 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-
-echo ⚡ Building Main Project...
-call mvn clean package -DskipTests -q
-if %ERRORLEVEL% NEQ 0 ( echo ❌ Benchmark failed. & pause & exit /b %ERRORLEVEL% )
-
+echo [FastFileIndex] Running Demo (via JitPack)...
 echo 🚀 Running Demo...
 cd examples\Demo
-call mvn compile exec:java -Dexec.mainClass=fastfileindex.Demo -q
-if %ERRORLEVEL% NEQ 0 ( echo ❌ Benchmark failed. & pause & exit /b %ERRORLEVEL% )
+call mvn -U compile exec:java -Dexec.mainClass=fastfileindex.Demo -q
+if %ERRORLEVEL% NEQ 0 ( echo ❌ Demo failed. & pause & exit /b %ERRORLEVEL% )
 
 cd ..\..
 pause
